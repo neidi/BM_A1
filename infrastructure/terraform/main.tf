da8d1c4d47349769602a89c4c782700f452c55ee
Simon Neidhart <simon.neidhart@hotmail.com>
2025-09-01 09:19:54 +0200

    add tutorial files

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,32 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.42.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "Switzerland North"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "Switzerland North"
+  resource_group_name = azurerm_resource_group.rg.name
+}
