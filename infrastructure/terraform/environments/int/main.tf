fd39b851d5ab0f3c4fa23e2cec50aa9fa819466b
Simon Neidhart <simon.neidhart@hotmail.com>
2025-09-15 13:30:56 +0200

    introduce and use module for creating a webapp

--- infrastructure/terraform/environments/int/main.tf
+++ infrastructure/terraform/environments/int/main.tf
@@ -26,69 +26,31 @@ resource "azurerm_service_plan" "appserviceplan1" {
   sku_name            = "B1"
 }
 
-resource "azurerm_linux_web_app" "webapp1" {
-  name                = var.webapp1_name
-  location            = azurerm_resource_group.int.location
-  resource_group_name = azurerm_resource_group.int.name
-  service_plan_id     = azurerm_service_plan.appserviceplan1.id
-
-  site_config {
-    application_stack {
-      docker_image_name        = "samples/todo-app:amd64"
-      docker_registry_url      = var.docker_registry_url
-      docker_registry_username = var.docker_registry_username_int
-      docker_registry_password = var.docker_registry_password_int
-    }
-  }
-
-  tags = {
-    Environment = "int"
-    Team        = "DevOps"
-  }
-  logs {
-    detailed_error_messages = true
-    failed_request_tracing  = true
-    http_logs {
-      file_system {
-        retention_in_days = 0
-        retention_in_mb   = 35
-      }
-    }
-  }
+module "webapp1" {
+  source                  = "../../modules/webapp"
+  name                    = var.webapp1_name
+  location                = azurerm_resource_group.int.location
+  resource_group_name     = azurerm_resource_group.int.name
+  service_plan_id         = azurerm_service_plan.appserviceplan1.id
+  docker_image_name       = "samples/todo-app:amd64"
+  docker_registry_url     = var.docker_registry_url
+  docker_registry_username = var.docker_registry_username_int
+  docker_registry_password = var.docker_registry_password_int
+  app_settings            = {}
 }
 
 
-resource "azurerm_linux_web_app" "webapp2" {
-  name                = var.webapp2_name
-  location            = azurerm_resource_group.int.location
-  resource_group_name = azurerm_resource_group.int.name
-  service_plan_id     = azurerm_service_plan.appserviceplan1.id
-
-  site_config {
-    application_stack {
-      docker_image_name        = "samples/todo-app-frontend:amd64-beta001"
-      docker_registry_url      = var.docker_registry_url
-      docker_registry_username = var.docker_registry_username_int
-      docker_registry_password = var.docker_registry_password_int
-    }
-  }
-
+module "webapp2" {
+  source                  = "../../modules/webapp"
+  name                    = var.webapp2_name
+  location                = azurerm_resource_group.int.location
+  resource_group_name     = azurerm_resource_group.int.name
+  service_plan_id         = azurerm_service_plan.appserviceplan1.id
+  docker_image_name       = "samples/todo-app-frontend:amd64-beta001"
+  docker_registry_url     = var.docker_registry_url
+  docker_registry_username = var.docker_registry_username_int
+  docker_registry_password = var.docker_registry_password_int
   app_settings = {
-    NEXT_PUBLIC_API_BASE_URL = "https://${azurerm_linux_web_app.webapp1.default_hostname}"
-  }
-
-  tags = {
-    Environment = "int"
-  }
-
-  logs {
-    detailed_error_messages = true
-    failed_request_tracing  = true
-    http_logs {
-      file_system {
-        retention_in_days = 0
-        retention_in_mb   = 35
-      }
-    }
+    NEXT_PUBLIC_API_BASE_URL = "https://${module.webapp1.default_hostname}"
   }
 }
